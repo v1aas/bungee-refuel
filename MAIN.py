a74c540de4b35dc5be22c21a794d8ec2b2334a19
Andrey <116958403+v1aas@users.noreply.github.com>
2023-09-25 20:11:19 +0300

    <new><Добавить новые сети, рефакторинг кода>
    Добавил новые сети, удалил fantom, добавить утилитарный файл config

--- MAIN.py
+++ MAIN.py
@@ -1,25 +1,25 @@
 from web3 import Web3
 from custom_logger import logger
+from config import RPC_LIST, CONTRACT_LIST, MAX_TIME, MIN_TIME
 import json
 import random
 import time
 
-POLYGON_RPC = 'https://polygon.blockpi.network/v1/rpc/public'
-AVAX_RPC = 'https://ava-mainnet.public.blastapi.io/ext/bc/C/rpc'
-BSC_RPC = 'https://bsc-dataseed.binance.org'
-BUNGEE_CONTRACT_POLYGON = '0xAC313d7491910516E06FBfC2A0b5BB49bb072D91'
-BUNGEE_CONTRACT_AVAX = '0x040993fbF458b95871Cd2D73Ee2E09F4AF6d56bB'
-BUNGEE_CONTRACT_BSC = '0xBE51D38547992293c89CC589105784ab60b004A9'
 CHAIN_ID = {
-    'bsc': 56,
-    'avax': 43114,
-    #'fantom': 250,
-    'polygon': 137
+    #'bsc': 56,
+    #'avax': 43114,
+    #'polygon': 137,
+    'arbitrum': 42161,
+    #'optimism': 10,
+    'zksync': 324,
+    #'polygon_zkevm': 1101,
+    #'base': 8453
     }
 
 def get_bungee_abi():
     with open('bungee_abi.txt', 'r') as file:
         return json.loads(file.readline().strip())
+    
 
 def get_private_keys():
     with open('private_keys.txt', 'r') as file:
@@ -27,17 +27,25 @@ def get_private_keys():
 
 def refuel(network, private_keys, amount):
     if network == 'polygon':
-        rpc = POLYGON_RPC
-        contract_address = BUNGEE_CONTRACT_POLYGON
+        rpc = RPC_LIST['polygon']
+        contract_address = CONTRACT_LIST['contract_polygon']
         target_chain_id = 137
     elif network == 'avax':
-        rpc = AVAX_RPC
-        contract_address = BUNGEE_CONTRACT_AVAX
+        rpc = RPC_LIST['avax']
+        contract_address = CONTRACT_LIST['contract_avax']
         target_chain_id = 43114
     elif network == 'bsc':
-        rpc = BSC_RPC
-        contract_address = BUNGEE_CONTRACT_BSC
+        rpc = RPC_LIST['bsc']
+        contract_address = CONTRACT_LIST['contract_bsc']
         target_chain_id = 56
+    elif network == 'arbitrum':
+        rpc = RPC_LIST['arbitrum']
+        contract_address = CONTRACT_LIST['contract_arbitrum']
+        target_chain_id = 42161
+    elif network == 'optimism':
+        rpc = RPC_LIST['optimism']
+        contract_address = CONTRACT_LIST['contract_optimism']
+        target_chain_id = 10
     else:
         logger.error("Такой сети нет")
         return
@@ -54,14 +62,18 @@ def refuel(network, private_keys, amount):
             logger.info(f'Начинаю рефуел из {network} в {chain}')
             transaction = create_transaction(contract, wallet, chain_id, target_chain_id, web3, amount)
             send_transaction(transaction, private_key, web3)
-            sleep = random.randint(10,30)
+            sleep = random.randint(MIN_TIME, MAX_TIME)
             logger.info(f'Сплю {sleep} секунд перед следующей транзакцией')
             time.sleep(sleep)
 
 def create_transaction(contract, wallet, chain_id, target_chain_id ,web3, amount):
     amount *= (1 + random.uniform(0, 0.1))
+    if target_chain_id == 42161:
+        gas = 730223
+    else:
+        gas = 30000
     return contract.functions.depositNativeToken(chain_id, wallet).build_transaction({
-        'gas': 68694,
+        'gas': gas,
         'gasPrice': web3.eth.gas_price,
         'value': Web3.to_wei(amount, 'ether'),
         'nonce': web3.eth.get_transaction_count(wallet),
@@ -83,7 +95,9 @@ def main():
 
     while True:
         print("Для выхода введи 0")
-        network = input("Из какой сети будет рефуел? \n 1 - polygon \n 2 - avax \n 3 - bsc \n")
+        network = input("Из какой сети будет рефуел?" + 
+                        "\n 1 - polygon \n 2 - avax \n 3 - bsc \n 4 - arbitrum \n 5 - optimism \n" + 
+                        "Введи число: ")
         if network == '0':
             break
         elif network == '1':
@@ -95,19 +109,29 @@ def main():
         elif network == '3':
             network = 'bsc'
             default_amount = 0.004
+        elif network == '4':
+            network = 'arbitrum'
+            default_amount = 0.0025
+        elif network == '5':
+            network = 'optimism'
+            default_amount = 0.0027
         else:
-            print("Неправильный ввод. Доступны команды: 0, 1, 2")
+            print("Неправильный ввод. Доступны команды: 0, 1, 2, 3, 4, 5")
             continue
 
-        amount = input("Сколько нативного токена рефулить? \n По умолчанию для матика отправляется 1 $MATIC\n"
-                       + " Для авакса 0.0065 $AVAX \n Для bsc 0.004 $BNB \n" + 
-                       "Оставь строку пустой или напиши своё количество: ")
+        amount = input("\n" + "Сколько нативного токена рефулить? По умолчанию отправляется: \n" +  
+                        "MATIC: 1 $MATIC\n" +
+                        "AVAX: 0.0065 $AVAX\n" +
+                        "BSC: 0.004 $BNB \n" +
+                        "ARBITRUM: 0.0025 $ETH \n" +
+                        "OPTIMISM: 0.0027 $ETH \n" + 
+                        "Оставь строку пустой или введи своё количество: ")
         amount = float(amount) if amount else default_amount
 
         refuel(network, private_keys, amount)
         logger.success("Скрипт завершил работу")
-        time.sleep(600)
         break
 
 if __name__ == "__main__":
     main()
+    
---
+++ config.py
@@ -0,0 +1,18 @@
+MIN_TIME = 10
+MAX_TIME = 30
+
+RPC_LIST = {
+    'polygon': 'https://polygon.blockpi.network/v1/rpc/public',
+    'avax': 'https://ava-mainnet.public.blastapi.io/ext/bc/C/rpc',
+    'bsc': 'https://bsc-dataseed.binance.org',
+    'arbitrum': 'https://arbitrum.llamarpc.com',
+    'optimism': 'https://optimism.llamarpc.com'
+    }
+
+CONTRACT_LIST = {
+    'contract_polygon' : '0xAC313d7491910516E06FBfC2A0b5BB49bb072D91',
+    'contract_avax' : '0x040993fbF458b95871Cd2D73Ee2E09F4AF6d56bB',
+    'contract_bsc' : '0xBE51D38547992293c89CC589105784ab60b004A9',
+    'contract_arbitrum' : '0xc0E02AA55d10e38855e13B64A8E1387A04681A00',
+    'contract_optimism' : '0x5800249621DA520aDFdCa16da20d8A5Fc0f814d8'
+}
